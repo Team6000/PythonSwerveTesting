4328e5cfc7dd4aad27e09856380dc418f482158f
BK-ShaliAcc <2026.b.kolieb@shalhevet.org>
2025-04-30 15:05:11 -0700

    Added drive forward function (not tested)

--- robotcontainer.py
+++ robotcontainer.py
@@ -61,7 +61,6 @@ class RobotContainer:
         instantiating a :GenericHID or one of its subclasses (Joystick or XboxController),
         and then passing it to a JoystickButton.
         """
-        #TODO: ADD DRIVE FORWARD BUTTON
 
         xButton = self.driverController.button(XboxController.Button.kX)
         xButton.onTrue(ResetXY(x=0.0, y=0.0, headingDegrees=0.0, drivetrain=self.robotDrive))
@@ -78,6 +77,13 @@ class RobotContainer:
         move_to_pose = RunCommand(lambda: PathtoPose(Pose2d(10,10,Rotation2d(0)), self.robotDrive))
         aButton.whileTrue(move_to_pose)
 
+        #TODO: TEST. HAVE IT COMMENTED OUT SO WE CAN DO ONE AT A TIME
+        """
+        # Drives Forward
+        bButton = self.driverController.button(XboxController.Button.kB)
+        move_forward = RunCommand(lambda: self.robotDrive.drive(0.2, 0, 0, True, True, True))
+        bButton.whileTrue(move_forward)        
+    """
 
 
     def disablePIDSubsystems(self) -> None:
